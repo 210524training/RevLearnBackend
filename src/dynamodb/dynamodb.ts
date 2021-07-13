ce957d0db3d9f706c6f1715d8eb7c5288d801191
kirk simmonds <aldrich19@gmail.com>
2021-07-13 19:04:52 -0400

    dynamodb change

--- src/dynamodb/dynamodb.ts
+++ src/dynamodb/dynamodb.ts
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');
 
 const dynamo = new AWS.DynamoDB.DocumentClient({
   region: 'us-west-2',
-  endpoint: 'http://dynamodb.us-west-2',
+  endpoint: 'http://dynamodb.us-west-2.amazonaws.com',
   apiVersion: 'latest',
 });
 
