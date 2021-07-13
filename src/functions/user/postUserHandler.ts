2c9b81fd66e9104871c1f9391b8a09113c1e2f09
kirk simmonds <aldrich19@gmail.com>
2021-07-13 19:03:17 -0400

    added user repo

--- src/functions/user/postUserHandler.ts
+++ src/functions/user/postUserHandler.ts
@@ -1,28 +1,19 @@
 import 'source-map-support/register';
 
-import type { ValidatedEventAPIGatewayProxyEvent } from '../../libs/apiGateway';
+import { formatJSONResponse, ValidatedEventAPIGatewayProxyEvent } from '../../libs/apiGateway';
 
 import { middyfy } from '../../libs/lambda';
-import {postUserSchema} from './schema';
+import { postUserSchema } from './schema';
 
 import userService from '../../services/user.service';
 import User from '../../models/User';
 
 const postUser: ValidatedEventAPIGatewayProxyEvent<typeof postUserSchema> = async (event) => {
-const user: User = event.body as User;
+  const user: User = event.body as User;
 
-  userService.addUser(user);
+  const result: boolean = await userService.addUser(user);
 
-  const response = {
-    statusCode: 201,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
-    },
-    body: JSON.stringify(user),
-  };
-
-  return response;
+  return formatJSONResponse(result ? 201 : 500, result);
 };
 
 // eslint-disable-next-line import/prefer-default-export
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,1 +1,21 @@
-import { documentClient } from 'aws-sdk/client/dynamodb';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import dynamo from '../dynamodb/dynamodb';
+import User from '../models/User';
+
+class UserRepository {
+  constructor(
+    private docClient = dynamo,
+  ) {}
+
+  async addUser(user: User): Promise<boolean> {
+    const params: DocumentClient.PutItemInput = {
+      TableName: 'RL_Users',
+      Item: user,
+    };
+
+    return this.docClient.put(params).promise()
+      .catch((error) => { console.log(error); return false; })
+      .then((result) => { console.log(result); return true; });
+  }
+}
+export default new UserRepository();
